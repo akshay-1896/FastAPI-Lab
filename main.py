ddab1c608d1bf276da0f927bf1f5f62d9a01240d
Akshay Sharma <officialakshay1896@gmail.com>
2025-12-25 14:52:34 +0530

    HTTP Methods Intro

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as file:
+        data = json.load(file)
+        
+    return data
+
 @app.get("/")
 def hello():
-    return {'message': 'Hello, World!'}
+    return {'message': 'Patient Management System API'}
 
 @app.get("/about")
 def about():
-    return {'message' : 'This is my first FastAPI application.'}
+    return {'message' : 'This API is designed to manage patient records efficiently.'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
