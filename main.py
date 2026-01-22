5dfcaa10bed3eccea92df75197071299d8ea148b
Akshay Sharma <officialakshay1896@gmail.com>
2026-01-22 22:21:24 +0530

    Query Params

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
 
+
 def load_data():
     with open('patients.json', 'r') as file:
         data = json.load(file)
-        
+
     return data
+
 
 @app.get("/")
 def hello():
     return {'message': 'Patient Management System API'}
 
+
 @app.get("/about")
 def about():
-    return {'message' : 'This API is designed to manage patient records efficiently.'}
+    return {'message': 'This API is designed to manage patient records efficiently.'}
+
 
 @app.get("/view")
 def view():
     data = load_data()
     return data
+
 
 @app.get('/patient/{patient_id}')
 def view_patient(patient_id: str = Path(..., description='The ID of the patient to retrieve', example='P001')):
@@ -29,5 +34,30 @@ def view_patient(patient_id: str = Path(..., description='The ID of the patient 
 
     if patient_id in data:
         return data[patient_id]
-    raise HTTPException(status_code=404, detail= 'Patient not found')
-        
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='Sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400, detail=f'Invalid field selected from {valid_fields}')
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(
+            status_code=400, detail='Order must be either asc or desc')
+
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(
+        data.values(),
+        key=lambda x: x.get(sort_by, 0),
+        reverse=sort_order
+    )
+
+    return sorted_data
